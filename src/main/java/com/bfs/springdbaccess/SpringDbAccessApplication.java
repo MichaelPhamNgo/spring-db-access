5d02b1859253127f9d62438731b01c069f7829b4
Phuc Pham N <michaelphamn@yahoo.com>
2022-03-09 13:36:33 -0800

    Initial commit

---
+++ src/main/java/com/bfs/springdbaccess/SpringDbAccessApplication.java
@@ -0,0 +1,13 @@
+package com.bfs.springdbaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbAccessApplication.class, args);
+    }
+
+}
